5dd91bc7e16402e71b0400b74da3d48d5e85a4a5
luqie0106 <luchenrui007@163.com>
2026-04-02 14:40:53 +0800

    Cleanup: Move old versions to versions/ and rename main script

--- get_camera.js
+++
@@ -1,8 +0,0 @@
-navigator.mediaDevices.enumerateDevices()
-  .then(devices => {
-    devices.forEach(device => {
-      if (device.kind === 'videoinput') {
-        console.log(`设备名: ${device.label}, ID: ${device.deviceId}`);
-      }
-    });
-  });
