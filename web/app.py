b04943e825c7e1236e52dd22e53e5beb94658eb2
luqie0106 <luchenrui007@163.com>
2026-04-14 10:26:03 +0800

    修改默认端口号从5000更改为5001，防止mac运行不兼容

--- web/app.py
+++ web/app.py
@@ -52,5 +52,5 @@ def clear_events():
 
 if __name__ == "__main__":
     host = os.getenv("FALL_WEB_HOST", "127.0.0.1")
-    port = int(os.getenv("FALL_WEB_PORT", "5000"))
+    port = int(os.getenv("FALL_WEB_PORT", "5001"))
     app.run(host=host, port=port, debug=False)
